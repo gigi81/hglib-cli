068ce8fa12b62385585f73f23096d20ffd8d3734
Levi Bard <levi@unity3d.com>
2011-07-08 11:48:29 +0200

    Add LGPL.

--- Mercurial/AssemblyInfo.cs
+++ Mercurial/AssemblyInfo.cs
@@ -1,3 +1,24 @@
+// 
+//  AssemblyInfo.cs
+//  
+//  Author:
+//       Levi Bard <levi@unity3d.com>
+//  
+//  Copyright (c) 2011 Levi Bard
+// 
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+// 
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
@@ -9,7 +30,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("levi")]
+[assembly: AssemblyCopyright("Levi Bard")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
